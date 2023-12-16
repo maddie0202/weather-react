2f877c6bdbd22cb79724fdd4500989ac424b33bb
maddie0202 <148143460+maddie0202@users.noreply.github.com>
2023-12-16 23:41:08 +0330

    added westher.css

--- src/Weather.js
+++ src/Weather.js
@@ -6,22 +6,22 @@ export default function Weather(){
         <div className='Weather'>
             
             <form>
-                <input type='search' placeholder='Enter a City' className='form-control ' />
-                <input type='submit' value='search' className='btn btn-primary ' /> 
+                <input type='search' placeholder='Enter a City' className='form-control' />
+                <input type='submit' value='search' className='btn btn-primary' /> 
             </form>
             
             <h1>Paris</h1>
-            <div className='row'>
+            <div className='row details' >
                 <div className='col-12'>Saturday 19:00</div>
                 <div className='col-12'>Clear with periodic clouds</div>
             </div>
             <div className='row'>
                 <div className='col-6'>
                     <img src='https://ssl.gstatic.com/onebox/weather/64/sunny_s_cloudy.png' alt='Cloudy' />
-                    5°C
+                    <span className='temperature'>5</span><span className='unit'>°C</span>
                 </div>
                 <div className='col-6'>
-                    <div className='row'>
+                    <div className='row details'>
                         <div className='col-12'>Precipitation: 0%</div>
                         <div className='col-12'>Humidity: 85%</div>
                         <div className='col-12'>Wind: 8 km/h</div>
